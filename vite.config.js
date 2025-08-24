3c1e13eeee312cf4bf1ba7587dd6015840ef0d10
Nagavishnu Karthik B S <nagavishnukarthikbs@gmail.com>
2025-08-24 18:13:00 +0530

    Some Changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
+  server: {
+    port: 4000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
